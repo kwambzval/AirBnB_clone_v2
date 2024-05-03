7842d07e8624dcff3bfa662a9646f1e699c63d92
kwambzval <valentinevkkv@gmail.com>
2024-05-03 03:52:23 -0700

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack, and distributes it
+to your web servers, using the function do_deploy.
+"""
+
+from fabric.api import env, put, run, local
+from datetime import datetime
+from os.path import exists
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+
+    # create a versions directory if it doesn't exist
+    local("mkdir -p versions")
+
+    # create a tgz archive of the web_static directory
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(time)
+    result = local("tar -cvzf {} web_static".format(archive_path))
+
+    # return the archive path if the archive has been correctly generated
+    if result.succeeded:
+        return archive_path
+    else:
+        return None
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers.
+    """
+
+    # return False if the file at the path archive_path doesn’t exist
+    if not exists(archive_path):
+        return False
+
+    try:
+        # upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # uncompress the archive to the folder /data/web_static/releases/ on the web server
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name))
+        # delete the archive from the web server
+        run("rm /tmp/{}".format(file_name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        # delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+        # create a new the symbolic link /data/web_static/current on the web server
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+        return True
+    except:
+        return False
+
